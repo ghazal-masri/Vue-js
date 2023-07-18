6bdfe5302b93b8dc846be67a9adc1155fc58eab1
Ghazal masri <78875165+ghazal-masri@users.noreply.github.com>
2023-07-18 23:58:14 +0300

    first vue proj

---
+++ modal-proj/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <div v-if="showmod">
+    <Modal theme="sale" @close="togglemod">
+      <template v-slot:links>
+        <a href="#">Learn more</a>
+        <a href="#">About Us</a>
+      </template>
+      <h1>Here is Web</h1>
+      <p>Get the Chance NOW!</p>
+    </Modal>
+  </div>
+  <button @click="togglemod">Open Modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "LOL!",
+      showmod: false,
+    };
+  },
+
+  methods: {
+    togglemod() {
+      this.showmod = !this.showmod;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-proj/src/components/Modal.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="backd" @click.self="closemod">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>Default</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closemod() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.backd {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+h1 {
+  border-radius: 10px;
+  color: aquamarine;
+}
+p {
+  font-style: normal;
+}
+.actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+.actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+.sale {
+  background: crimson;
+  color: white;
+}
+.sale h1 {
+  color: white;
+}
+.sale .actions a {
+  color: white;
+}
+</style>
